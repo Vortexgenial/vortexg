bdb43b103e2dd1d5aa3fca97cff55c1f4c5b73bd
Vorttex genial <MICHVERDE10@GMAIL.COM>
2024-12-13 12:59:50 -0600

    Update ver_c.php

--- ver_c.php
+++ ver_c.php
@@ -1,21 +1,21 @@
 <table border=1>
 <tr>
-<th>ID</th>
-<th>Nombre</th>
-<th>Correo</th>
-<th>Telefono</th>
+<th>id</th>
+<th>nombre</th>
+<th>correo</th>
+<th>telefono</th>
 </tr>
     <?php
     Function conectar(){
     $servername = "sql5.freesqldatabase.com";
-    $username = "sql5723954";
-    $password = "8bNKjjmTEj";
-    $dbname = "sql5723954";  
+    $username = "sql5751858";
+    $password = CUrU1KaUnQ";
+    $dbname = "sql5751858";  
 	$conexion = new mysqli($servername, $username, $password, $dbname);
 	return $conexion;
 }
         $conexion = conectar();
-        $query = $conexion->query("SELECT * from Usuariosg");
+        $query = $conexion->query("SELECT * from Vortexgs");
         $cols_num = 4; $matriz = array(); $f = 0;
         while($celda = $query->fetch_assoc()){$keys = array_keys($celda);for($c=0;$c<$cols_num;$c++){$matriz[$f][$c] = $celda[$keys[$c]];}$f++;}
         foreach ($matriz as $y){echo "<tr>";foreach ($y as $yx){echo "<td>".$yx."</td>";}echo "</tr>";}
